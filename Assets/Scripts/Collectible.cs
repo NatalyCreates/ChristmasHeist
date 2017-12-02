0fdb63884849a295709861597439761451f0b548
Michael Shalyt <michael@shalyt.com>
2017-12-02 16:16:53 +0200

    sdf

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,7 +4,38 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour {
 
-	// maybe not needed since we can just check a tag and then add to the collected count
+    // maybe not needed since we can just check a tag and then add to the collected count
     // ACTUALLY
     // randomize the texture for different stuff you collect
+
+    Collider noiseCollider;
+    NoiseRadius noise;
+
+    void Start()
+    {
+        // position player in entrance
+        // init other stuff
+        //noise = GetComponentInChildren<NoiseRadius>();
+        //noiseCollider = Player.noise.GetComponent<Collider>();
+        //noise = gameObject.GetComponent(typeof(NoiseRadius)) as NoiseRadius;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name == "Tree")
+        {
+            Debug.Log("Trigger Enter Collectible - was noise");
+        }
+        else
+        {
+            Debug.Log("Trigger Enter Collectible");
+            //Collectible dissapears
+            //Add 1 to number of coll
+            Player.Instance.numCollectibles++;
+        }
+
+    }
+
 }
