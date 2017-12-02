e051fcc1327685fedb62f765ef70c9dfbb1b5a7c
Michael Shalyt <michael@shalyt.com>
2017-12-02 14:15:57 +0200

    Added movement. Moved speed to GameDeisgn. PLEASE ADD GameDesign SCRIPT TO SOME OBJECT

--- Assets/Scripts/GameDesign.cs
+++ Assets/Scripts/GameDesign.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class GameDesign : MonoBehaviour {
 
-	// balance stuff here
+    // balance stuff here
     // movement speeds of player and guards
     // timers
     // radius of noise levels as a function of number of collectibles
     // ???
-}
+
+    public static GameDesign Instance;
+
+    public float playerSpeed = 8;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour {
 
     NoiseRadius noise;
 
-    public float movementSpeed;
+    internal Vector3 direction = Vector3.zero;
+    internal Vector3 direction_normalized;
+
+    internal float movementSpeed;
     int numCollectibles = 0;
 
 	void Start() {
@@ -15,8 +18,18 @@ public class Player : MonoBehaviour {
 	}
 	
 	void Update() {
-		// player input (4 dir movement)
+        // player input (4 dir movement)
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = 0;
+        direction.z = Input.GetAxis("Vertical");
+        direction_normalized = direction.normalized;
+        Debug.Log("IHAMAIMC direction vector " + direction.ToString());
+
+        transform.Translate(direction_normalized.x * GameDesign.Instance.playerSpeed * Time.deltaTime, direction.y, direction_normalized.z * GameDesign.Instance.playerSpeed * Time.deltaTime);
+
         // animate
+
         // activate noise radius only when the player is moving
-	}
+    }
 }
