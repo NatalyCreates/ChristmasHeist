023df8869a412f7d0a9fc68833460c3f03178f7b
NatalyCreates <nataly@natalycreates.com>
2017-12-02 23:01:17 +0200

    game loop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -9,6 +10,14 @@ public class GameManager : MonoBehaviour {
 
     public Text timer;
     public Text score;
+
+    public Text gameOverText;
+    public GameObject gameOverBanner;
+
+    float secondsLeft = 30;
+
+    internal bool isGameRunning = false;
+    bool isGameOver = false;
 
     void Awake()
     {
@@ -26,11 +35,52 @@ public class GameManager : MonoBehaviour {
 	void Start() {
         score.text = "0";
         timer.text = "00:00";
+        StartCoroutine(StartGameDelayed(2f));
 	}
+    IEnumerator StartGameDelayed(float time)
+    {
+        yield return new WaitForSeconds(time);
+        isGameRunning = true;
+    }
 	
 	void Update() {
-		
+        timer.text = (Mathf.RoundToInt(secondsLeft) / 60).ToString("N0") + ":" + (Mathf.RoundToInt(secondsLeft) % 60).ToString("N0");
+        if (isGameRunning)
+        {
+            secondsLeft -= Time.deltaTime;
+        }
+        if (secondsLeft <= 0)
+        {
+            isGameRunning = false;
+            gameOverText.text = "Out of Time!";
+            gameOverBanner.SetActive(true);
+            isGameOver = true;
+        }
+
+        if (isGameOver)
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
 	}
+
+    public void OnExitMall()
+    {
+        isGameRunning = false;
+        gameOverText.text = "Merry Christmas!";
+        gameOverBanner.SetActive(true);
+        isGameOver = true;
+    }
+
+    public void OnGuardCaught()
+    {
+        isGameRunning = false;
+        gameOverText.text = "Busted!";
+        gameOverBanner.SetActive(true);
+        isGameOver = true;
+    }
 
     public void UpdateScore(int newScore)
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,35 +29,37 @@ public class Player : MonoBehaviour {
     }
 	
 	void Update() {
-        // player input (4 dir movement)
-
-        direction.x = Input.GetAxis("Horizontal");
-        direction.y = 0;
-        direction.z = Input.GetAxis("Vertical");
-        direction_normalized = direction.normalized;
-        //Debug.Log("CH direction vector " + direction.ToString());
-
-        transform.Translate(direction_normalized.x * GameDesign.Instance.playerSpeed * Time.deltaTime, direction.y, direction_normalized.z * GameDesign.Instance.playerSpeed * Time.deltaTime);
-
-        // animate
-
-
-
-        // activate noise radius only when the player is moving
-        if (direction == Vector3.zero)
+        if (GameManager.Instance.isGameRunning)
         {
-            noise.GetComponent<Renderer>().enabled = false;
-            noise.GetComponent<Collider>().enabled = false;
-            //Debug.Log("Radius hidden");
-            //NoiseRadius.Instance.HideNoiseRadius();
-        }
-        else
-        {
-            noise.GetComponent<Renderer>().enabled = true;
-            noise.GetComponent<Collider>().enabled = true;
-            //Debug.Log("Radius shown");
-            //NoiseRadius.Instance.ShowNoiseRadius();
-        }
+            // player input (4 dir movement)
 
+            direction.x = Input.GetAxis("Horizontal");
+            direction.y = 0;
+            direction.z = Input.GetAxis("Vertical");
+            direction_normalized = direction.normalized;
+            //Debug.Log("CH direction vector " + direction.ToString());
+
+            transform.Translate(direction_normalized.x * GameDesign.Instance.playerSpeed * Time.deltaTime, direction.y, direction_normalized.z * GameDesign.Instance.playerSpeed * Time.deltaTime);
+
+            // animate
+
+
+
+            // activate noise radius only when the player is moving
+            if (direction == Vector3.zero)
+            {
+                noise.GetComponent<Renderer>().enabled = false;
+                noise.GetComponent<Collider>().enabled = false;
+                //Debug.Log("Radius hidden");
+                //NoiseRadius.Instance.HideNoiseRadius();
+            }
+            else
+            {
+                noise.GetComponent<Renderer>().enabled = true;
+                noise.GetComponent<Collider>().enabled = true;
+                //Debug.Log("Radius shown");
+                //NoiseRadius.Instance.ShowNoiseRadius();
+            }
+        }
     }
 }
