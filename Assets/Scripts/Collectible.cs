7e6f1c0270b7d17bab1d1cb461ea732e371a1454
NatalyCreates <nataly@natalycreates.com>
2017-12-04 15:43:25 +0200

    try lerp

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -25,7 +25,6 @@ public class Collectible : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.name == "Tree")
         {
             //Debug.Log("Trigger Enter Collectible - was Tree");
@@ -39,9 +38,12 @@ public class Collectible : MonoBehaviour {
             NoiseRadius.Instance.transform.localScale = new Vector3(0.5f, noiseRadScale, noiseRadScale);
 
             //Player.Instance.GetComponentInChildren<Rigidbody>().gameObject.transform.localScale *= 1.1f;
-            Transform t = Player.Instance.GetComponentInChildren<MeshRenderer>().gameObject.transform;
-            t.localScale = new Vector3(t.localScale.x + 0.03f, t.localScale.y + 0.03f, t.localScale.z + 0.03f);
-            t.position = new Vector3(t.position.x, t.position.y + 0.015f, t.position.z);
+            StopAllCoroutines();
+            StartCoroutine(LerpGrowShrinkTree());
+
+            //Transform t = Player.Instance.GetComponentInChildren<MeshRenderer>().gameObject.transform;
+            //Vector3 newScale = new Vector3(t.localScale.x + 0.08f, t.localScale.y + 0.08f, t.localScale.z + 0.08f);
+            //t.localScale = newScale;
 
             SoundManager.Instance.OnPickup();
             Destroy(gameObject);
@@ -55,4 +57,44 @@ public class Collectible : MonoBehaviour {
 
     }
 
+    IEnumerator LerpGrowShrinkTree() {
+        Transform t = Player.Instance.GetComponentInChildren<MeshRenderer>().gameObject.transform;
+        Vector3 newScale = new Vector3(t.localScale.x + 0.08f, t.localScale.y + 0.08f, t.localScale.z);
+        Vector3 newScalePlus = new Vector3(t.localScale.x + 1.5f, t.localScale.y + 1.5f, t.localScale.z);
+        Vector3 newPos = new Vector3(t.position.x, t.position.y + 0.04f, t.position.z);
+        Vector3 newPosPlus = new Vector3(t.position.x, t.position.y + 0.75f, t.position.z);
+
+        float totalTime = 0.25f;
+        float elapsedTime = 0f;
+        Vector3 initScale = t.localScale;
+        //localPosition
+        //Vector3 initPos = t.position;
+
+        while (elapsedTime < totalTime) {
+            t.localScale = Vector3.Lerp(initScale, newScale, (elapsedTime / totalTime));
+            elapsedTime += Time.deltaTime;
+            //t.position = Vector3.Lerp(initPos, newPosPlus, elapsedTime / totalTime);
+            yield return null;
+        }
+        if (elapsedTime >= totalTime) {
+            //t.localScale = newScalePlus;
+            //t.position = newPosPlus;
+        }
+        //t.localScale = newScale;
+
+        //yield return new WaitForSeconds(3f);
+
+        /*
+        elapsedTime = 0f;
+        while (elapsedTime < totalTime) {
+            elapsedTime += Time.deltaTime;
+            t.localScale = Vector3.Lerp(t.localScale, newScale, elapsedTime / totalTime);
+            //localPosition
+            t.position = Vector3.Lerp(t.position, newPos, elapsedTime / totalTime);
+            yield return null;
+        }
+        t.localScale = newScale;
+        t.position = newPos;
+        */
+    }
 }
